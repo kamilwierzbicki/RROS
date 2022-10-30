663f193bbecd3a8769974360fef69f918d85c198
Kamil Wierzbicki <kamil.wierzbicki@int2code.com>
2022-10-31 00:26:04 +0100

    Simplify RROS_run function

--- src/RROS.c
+++ src/RROS.c
@@ -14,6 +14,8 @@ RROS_Result RROS_init(RROS_Handle *const pHandle, RROS_Task *const pTasks,
 static RROS_Result RROS_validateTask(const RROS_Task *pTask);
 static void RROS_StoreErrorEvent(RROS_Handle *pHandle, uint32_t taskID,
                                  RROS_Result errorCode, uint32_t errorTime);
+
+static RROS_Result RROS_runTask(RROS_Handle *pHandle, RROS_Task *pTask);
 /*------------------------------------------------------*/
 /* ----------------Function Definitions---------------- */
 /*------------------------------------------------------*/
@@ -58,48 +60,25 @@ RROS_Result RROS_run(RROS_Handle *const pHandle)
     RROS_Result res = RROS_Result_OK;
     RROS_TRY
     {
-        if (pHandle == NULL)
+        if (pHandle == NULL || pHandle->tasks == NULL)
         {
             RROS_THROW(RROS_Result_InvalidParameters);
         }
+        RROS_Task *pTasks = pHandle->tasks;
+        size_t tasksNum = pHandle->numOfTasks;
         uint32_t currentTime = RROS_getCurrentTime();
-        for (uint32_t taskIndex = 0U; taskIndex < pHandle->numOfTasks;
-             taskIndex++)
+        for (uint32_t taskIndex = 0U; taskIndex < tasksNum; taskIndex++)
         {
-            if (currentTime >= pHandle->tasks[taskIndex].task_period +
-                                   pHandle->tasks[taskIndex]._lastRunTime)
+            if (currentTime >=
+                pTasks[taskIndex].task_period + pTasks[taskIndex]._lastRunTime)
             {
-                uint32_t taskStartTime = RROS_getCurrentTime();
-                uint32_t taskEndTime = 0U;
-                RROS_Result taskResult = RROS_Result_OK;
-                pHandle->tasks[taskIndex]._lastRunTime = currentTime;
-                taskResult = pHandle->tasks[taskIndex].task_run();
-                taskEndTime = RROS_getCurrentTime();
-                switch (taskResult)
-                {
-                case RROS_Result_TaskCriticalError:
-                    RROS_StoreErrorEvent(pHandle,
-                                         pHandle->tasks[taskIndex].task_ID,
-                                         taskResult, taskEndTime);
-                    RROS_THROW(taskResult);
-                    break;
-                case RROS_Result_TaskError:
-                    RROS_StoreErrorEvent(pHandle,
-                                         pHandle->tasks[taskIndex].task_ID,
-                                         taskResult, taskEndTime);
-                    break;
+                RROS_Result taskResult =
+                    RROS_runTask(pHandle, &(pTasks[taskIndex]));
 
-                default:
-                    break;
-                }
-                if (taskEndTime - taskStartTime >
-                    pHandle->tasks[taskIndex]
-                        .task_timeout) // TODO: Overflow protection
+                if (taskResult != RROS_Result_OK &&
+                    taskResult != RROS_Result_TaskError)
                 {
-                    RROS_StoreErrorEvent(pHandle,
-                                         pHandle->tasks[taskIndex].task_ID,
-                                         RROS_Result_TaskTimeout, taskEndTime);
-                    RROS_THROW(RROS_Result_TaskTimeout);
+                    RROS_THROW(taskResult);
                 }
             }
         }
@@ -137,4 +116,44 @@ static void RROS_StoreErrorEvent(RROS_Handle *pHandle, uint32_t taskID,
     pHandle->error.errorTime = errorTime;
     pHandle->error.task_ID = taskID;
     pHandle->error.taskErrorCode = errorCode;
+}
+
+static RROS_Result RROS_runTask(RROS_Handle *pHandle, RROS_Task *pTask)
+{
+    RROS_Result res = RROS_Result_OK;
+    RROS_TRY
+    {
+        uint32_t taskStartTime = RROS_getCurrentTime();
+        uint32_t taskEndTime = 0U;
+        RROS_Result taskResult = RROS_Result_OK;
+        pTask->_lastRunTime = taskStartTime;
+        taskResult = pTask->task_run();
+        taskEndTime = RROS_getCurrentTime();
+        switch (taskResult)
+        {
+        case RROS_Result_TaskCriticalError:
+            RROS_StoreErrorEvent(pHandle, pTask->task_ID, taskResult,
+                                 taskEndTime);
+            RROS_THROW(taskResult);
+            break;
+        case RROS_Result_TaskError:
+            RROS_StoreErrorEvent(pHandle, pTask->task_ID, taskResult,
+                                 taskEndTime);
+            break;
+
+        default:
+            break;
+        }
+        if (taskEndTime - taskStartTime >
+            pTask->task_timeout) // TODO: Overflow protection
+        {
+            RROS_StoreErrorEvent(pHandle, pTask->task_ID,
+                                 RROS_Result_TaskTimeout, taskEndTime);
+            RROS_THROW(RROS_Result_TaskTimeout);
+        }
+    }
+    RROS_CATCH_BEGIN(res)
+    {}
+    RROS_CATCH_END
+    return res;
 }
